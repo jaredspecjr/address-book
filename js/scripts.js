71826696200556f2277f3596ef90f323f4a80aa1
jared <jaredspecjr@gmail.com>
2018-04-01 14:06:08 -0700

    add ability to click on name given to show more information about said person

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+Contact.prototype.fullName = function(){
+  return this.firstName + " " + this.lastName;
+}
 
 $(document).ready(function(){
   $("form#new-contact").submit(function(event){
@@ -9,8 +12,14 @@ $(document).ready(function(){
     var inputtedFirstname = $("input#new-first-name").val();
     var inputtedLastname = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstname, inputtedLastname);
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $(".contact").last().click(function(){
+    $("#show-contact").show();
+    $("show-contact h2").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+  });
   });
 });
